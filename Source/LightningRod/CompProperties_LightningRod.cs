b9c83fbeac5fd3d6d95204f762d7d2d46d334f5d
Mlie <emipa606@users.noreply.github.com>
2024-03-23 21:52:29 +0100

    Mod updated for 1.5 and passed autotests

--- Source/LightningRod/CompProperties_LightningRod.cs
+++ Source/LightningRod/CompProperties_LightningRod.cs
@@ -4,14 +4,14 @@ namespace SSLightningRod;
 
 public class CompProperties_LightningRod : CompProperties_Power
 {
-    public float chargeCapacity = 2000.00f;
-    public float cooldownPercentPowerSave = 50;
-    public float cooldownSpeed = 1f;
-    public float fakeZIndex = 4f;
-    public bool isBasic = false;
-    public int oneInXChanceHitPowerSave = 4;
-    public float powerGainDischarge = 2000;
-    public bool strikesHitBase = false;
+    public readonly float chargeCapacity = 2000.00f;
+    public readonly float cooldownPercentPowerSave = 50;
+    public readonly float cooldownSpeed = 1f;
+    public readonly float fakeZIndex = 4f;
+    public readonly bool isBasic = false;
+    public readonly int oneInXChanceHitPowerSave = 4;
+    public readonly float powerGainDischarge = 2000;
+    public readonly bool strikesHitBase = false;
 
     public CompProperties_LightningRod()
     {
